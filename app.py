e6cbce9de208145ebe5c2a0335c2caa3c9ada93f
Andy2020code <144275177+Andy2020code@users.noreply.github.com>
2023-11-18 17:13:26 -0500

    design update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 # Add a route to serve your CSS file
 @app.route('/css/<filename>')
 def serve_css(filename):
-    return app.send_static_file('css/' + filename)#
+    return app.send_static_file('css/' + filename)
 
 # Add a route to serve your image files
 @app.route('/IMG/<filename>')
@@ -22,10 +22,21 @@ def serve_js(filename):
     return app.send_static_file('js/' + filename)
 
 # Add routes for specific HTML pages
-#@app.route('/our-services')
-#def serve_our_services():
-#    return render_template('our-services.html')
+@app.route('/index')
+def serve_index():
+    return render_template('index.html')
 
+@app.route('/services')
+def serve_our_services():
+    return render_template('services.html')
+
+@app.route('/webapp')
+def serve_therapists():
+    return render_template('web-app.html')
+
+@app.route('/take-action')
+def serve_take_action():
+    return render_template('take-action.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
