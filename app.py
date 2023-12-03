168c025bce6a009fa224d81628e81a6c08412ded
Andy2020code <144275177+Andy2020code@users.noreply.github.com>
2023-12-03 15:49:11 -0500

    design update

--- app.py
+++ app.py
@@ -58,6 +58,10 @@ def web_resume():
 def asset_update():
     return render_template('asset-library-01.html')
 
+@app.route('/photography')
+def photo_portfolio():
+    return render_template('photography-portfolio.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000)
