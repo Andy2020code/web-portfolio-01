280813674f8be4dce35e9833482677ea474ecb2a
Andy2020code <144275177+Andy2020code@users.noreply.github.com>
2023-12-04 11:47:30 -0500

    design update

--- static/js/photography.js
+++ static/js/photography.js
@@ -1,6 +1,7 @@
 const carousel = document.querySelector(".carousel"),
 firstImg = carousel.querySelectorAll("img")[0],
 arrowIcons = document.querySelectorAll(".wrapper i");
+
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 const showHideIcons = () => {
     // showing and hiding prev/next icon according to carousel scroll left value
